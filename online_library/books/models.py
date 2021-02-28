cf8b514ec1afdcff8a816ba323932e02785f6c9b
Vladislav <v.janibekyan@gmail.com>
2021-02-28 19:09:30 +0200

    reg user book view added

--- online_library/books/models.py
+++ online_library/books/models.py
@@ -19,7 +19,7 @@ class Book(models.Model):
     age_group = models.IntegerField(choices=GROUP_CHOICES)
     downloaded_times = models.IntegerField()
     rate = models.FloatField()
-    book_image = models.ImageField(upload_to='book_cover_iamge', default="image.png")
+    book_image = models.ImageField(upload_to='book_cover_image', default="image.png")
     pdf_file = models.FileField(default=True)
 
 
