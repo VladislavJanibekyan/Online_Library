8750f1a40db34e52488b560dc98e358199b1e14d
MilenTar <tarverdyanmilena@yandex.ru>
2021-03-03 18:25:47 +0400

    search added

--- online_library/books/urls.py
+++ online_library/books/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
+from .views import  SearchResultsView
+
 
 urlpatterns =[
     path('', views.home, name="books_home"),
     path('about/', views.about, name="about"),
     path('book_view/<int:pk>', views.book_view, name="book_view"),
+    path('search/', SearchResultsView.as_view(), name='search_results'),
 ]
--- online_library/books/views.py
+++ online_library/books/views.py
@@ -4,8 +4,24 @@ from django.contrib.auth.decorators import login_required
 import os
 from django.conf import settings
 from django.http import HttpResponse
+from django.views.generic import TemplateView, ListView
+from django.db.models import Q
+from django.contrib import messages
 
-# Create your views here.
+
+
+class SearchResultsView(ListView):
+    model = Book
+    template_name = 'search_results.html'
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        object_list = Book.objects.filter(
+            Q(title__icontains=query) | Q(category__icontains=query)| Q(author__icontains=query)
+        )
+        return object_list
+
+
 def home(request):
     book = Book.objects.all()
 
@@ -13,10 +29,12 @@ def home(request):
 
 def about(request):
     return render(request, 'books/about.html')
+
 @login_required
 def book_view(request, pk):
     specific_book = get_object_or_404(Book, pk=pk)
     return render(request, 'books/book_view.html', {'book': specific_book})
+
 @login_required()
 def download(request,path):
     file_path = os.path.join(settings.MEDIA_ROOT, path)
